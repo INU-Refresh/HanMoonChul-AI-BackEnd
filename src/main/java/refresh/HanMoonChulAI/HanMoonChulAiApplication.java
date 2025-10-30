f457c230e1f0377e4400bdbda0c265a75cb2924c
d6nggyun <d6nggyun@gmail.com>
2025-10-30 14:20:11 +0900

    ⚙️ setting: 초기 파일 세팅

---
+++ src/main/java/refresh/HanMoonChulAI/HanMoonChulAiApplication.java
@@ -0,0 +1,13 @@
+package refresh.HanMoonChulAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanMoonChulAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanMoonChulAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/refresh/HanMoonChulAI/HanMoonChulAiApplicationTests.java
@@ -0,0 +1,13 @@
+package refresh.HanMoonChulAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanMoonChulAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
